d66d6016462e7d0ee350244baaf7deb806a76cda
ochid23 <hidayatnurrosyid1982@gmail.com>
2026-04-01 12:01:03 +0700

    Migrate codebase from Laravel to Next.js framework

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import 'remixicon/fonts/remixicon.css';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'BTM Invoice Generator',
+  description: 'Aplikasi Invoice Jasa Transportasi Bagas Trans Mandiri',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="id">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
